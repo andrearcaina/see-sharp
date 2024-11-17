e2e672e1c4b186b39be2c13724519e1300320e88
andrearcaina <dtandre331@gmail.com>
2024-11-16 20:41:39 -0500

    variables + c# 10

---
+++ variables/main.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace variables
+{
+    public class main
+    {
+        static void Main(string[] args)
+        {
+            // just like Java
+            int myInt = 10;
+            double myDouble = 10.5;
+            char myChar = 'A';
+            string myString = "Hello World!";
+            bool myBool = true;
+
+            // comments wow
+            Console.WriteLine("myInt: " + myInt);
+            Console.WriteLine("myDouble: " + myDouble);
+            Console.WriteLine("myChar: " + myChar);
+            Console.WriteLine("myString: " + myString);
+            Console.WriteLine("myBool: " + myBool);
+        }
+    }
+}
