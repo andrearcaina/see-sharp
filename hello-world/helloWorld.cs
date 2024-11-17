9e2c5200364fe85071b2a16bb4cfcc18c3e3623a
andrearcaina <dtandre331@gmail.com>
2024-11-16 20:30:14 -0500

    insane learning happening right now

---
+++ hello-world/helloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    public class helloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }    
+}
