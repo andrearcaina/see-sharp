fff364a76b955106b0ab2b6346b3baf6bdb51275
andrearcaina <dtandre331@gmail.com>
2025-01-25 11:57:20 -0500

    created simple TodoAPI
    followed microsoft web api tutorial with ASP.NET
    simple .NET web api built in MVC architecture
    utilized scaffolding and code gen to create a controller

---
+++ TodoApi/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
